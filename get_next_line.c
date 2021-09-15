2c4328deeff975f3c4ddd87cc7272552654a3186
William Moura <felipe.1.william@gmail.com>
2021-09-15 01:37:00 -0300

    first test

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,9 @@
 /*   By: wfelipe- < wfelipe-@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 19:19:22 by wfelipe-          #+#    #+#             */
-/*   Updated: 2021/09/15 01:06:19 by wfelipe-         ###   ########.fr       */
+/*   Updated: 2021/09/15 01:36:37 by wfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
-//Se não há nada para ler, retorna nulo.
 
 //Garanta que sua função se comporta bem tanto lendo um arquivo quanto lendo um input padrão.
 
@@ -28,20 +25,19 @@
 //7. Se a leitura não foi bem sucedida (read = 0), faça uma maracutaia que não sei o que é
 #include "get_next_line.h"
 
-
-char *breakline_founded(char **line_that_is_be_readen, char *auxiliar)
+char	*breakline_founded(char **line_that_is_be_readen, char *auxiliar)
 {
 	size_t	index;
-	char *missed_buffer;
-	char *ponteiro;
+	char	*missed_buffer;
+	char	*ponteiro;
 
 	index = 0;
-	while(*(*line_that_is_be_readen + index) != '\n')
+	while (*(*line_that_is_be_readen + index) != '\n')
 		++index;
 	ponteiro = *(line_that_is_be_readen) + index + 1;
 	auxiliar = ft_calloc(ft_strlen(*line_that_is_be_readen) - ft_strlen(ponteiro) + 1, sizeof(char));
 	ft_memmove(auxiliar, *line_that_is_be_readen, ft_strlen(*line_that_is_be_readen) - ft_strlen(ponteiro));
-	if(ft_strncmp(*line_that_is_be_readen, auxiliar, ft_strlen(*line_that_is_be_readen)) == 0)
+	if (ft_strncmp(*line_that_is_be_readen, auxiliar, ft_strlen(*line_that_is_be_readen)) == 0)
 	{
 		free(*line_that_is_be_readen);
 		*line_that_is_be_readen = NULL;
@@ -64,15 +60,15 @@ void	freed_and_nulled(char **string)
 	return ;
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	static char	*line_that_is_be_readen = NULL;
-	char 		*auxiliar;
-	int			end_file_identifier;//essa é só uma forma de identificar que foi a última leitura do arquivo.
+	char		*auxiliar;
+	int			end_file_identifier;
 
 	auxiliar = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	end_file_identifier = read(fd, auxiliar, BUFFER_SIZE);
-	if(fd < 0 || (!*auxiliar && !line_that_is_be_readen) || !auxiliar)
+	if (fd < 0 || (!*auxiliar && !line_that_is_be_readen) || !auxiliar || read(fd, auxiliar, 0))
 	{
 		freed_and_nulled(&auxiliar);
 		return (NULL);
@@ -80,19 +76,19 @@ char *get_next_line(int fd)
 	if (line_that_is_be_readen && *auxiliar)
 		line_that_is_be_readen = ft_strjoin_modified(&line_that_is_be_readen, &auxiliar);
 	else if (*auxiliar)
-		line_that_is_be_readen = ft_strdup_modified(auxiliar);//dou free na auxiliar na própria dup
+		line_that_is_be_readen = ft_strdup_modified(auxiliar);
 	freed_and_nulled(&auxiliar);
 	if (line_that_is_be_readen && ft_strchr(line_that_is_be_readen, '\n'))
 		return (breakline_founded(&line_that_is_be_readen, auxiliar));
-	else if(end_file_identifier == 0 && line_that_is_be_readen)
+	else if (end_file_identifier == 0 && line_that_is_be_readen)
 	{
 		auxiliar = ft_strdup_modified(line_that_is_be_readen);
 		freed_and_nulled(&line_that_is_be_readen);
-		return(auxiliar);
+		return (auxiliar);
 	}
-	return(get_next_line(fd));
+	return (get_next_line(fd));
 }
-
+/*
 int	main (void)
 {
 	int	fd;
@@ -109,4 +105,4 @@ int	main (void)
 	// }
 	free(variavel);
 	return (0);
-}
+}*/
